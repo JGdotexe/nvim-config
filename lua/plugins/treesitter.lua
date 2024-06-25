fbc99458c392d48e4d217d3a1f2c024f6cf6a20e
JGdotexe <jfreirasgoulart@gmail.com>
2024-06-25 14:37:21 -0300

    initial commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-treesitter/nvim-treesitter", 
+	build = ":TSUpdate",
+	config =  function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+		  ensure_installed = { "c", "lua","javascript", "html", "css", "bash" },
+		  sync_install = false,
+		  highlight = { enable = true },
+		  indent = { enable = true },  
+		})
+	end
+}
