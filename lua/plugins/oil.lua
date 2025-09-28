a9697ff08e5928a4f3ca6495c1eb44127422802e
jg <jfreitasgoulart@gmail.com>
2025-09-27 23:54:01 -0300

    changes on oil and search file

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,9 +2,20 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.setupopts
-  opts = {},
+  opts = {
+    -- Options for how the file listing is displayed
+    view_options = {
+      -- This will show hidden files (like .gitignore)
+      show_hidden = true,
+      -- This is how we sort directories first
+      sort = {
+        -- First, sort by type (directories vs files) in ascending order
+        { "type", "asc" },
+        -- Then, sort by name in ascending (alphabetical) order
+        { "name", "asc" },
+      },
+    },
+  },
   -- optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
-
